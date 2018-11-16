629338688622399bd7bce6eb2d311ed4369eeb53
Kevin Hoppel <kevin.hoppel@gmail.com>
2018-11-16 12:48:38 -0500

    JSX Expressions

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'Larry';
+    const showHello = false;
+    const showMath = true;
+    const num1 = 40;
+    const num2 = 23;
+
+    let math;
+    if (showMath) {
+      math = (
+        <h4>
+          {num1} + {num2} = {num1 + num2}
+        </h4>
+      );
+    } else {
+      math = null;
+    }
+
     return (
       <div className="App">
         <h1>The App Component</h1>
+        {showHello ? <h4>Hello {name}</h4> : null}
+        {math}
       </div>
     );
   }
