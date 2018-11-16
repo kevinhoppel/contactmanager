e29f7caa89f4ae193c3ddd12c43e561fea187135
Kevin Hoppel <kevin.hoppel@gmail.com>
2018-11-16 12:54:13 -0500

    Create new component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Larry';
-    const showHello = false;
-    const showMath = true;
-    const num1 = 40;
-    const num2 = 23;
-
-    let math;
-    if (showMath) {
-      math = (
-        <h4>
-          {num1} + {num2} = {num1 + num2}
-        </h4>
-      );
-    } else {
-      math = null;
-    }
-
     return (
       <div className="App">
         <h1>The App Component</h1>
-        {showHello ? <h4>Hello {name}</h4> : null}
-        {math}
+        <Contact />
       </div>
     );
   }
