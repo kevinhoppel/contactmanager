47ccb7c1a65087ea9f42d926c2742affd2cbd729
Kevin Hoppel <kevin.hoppel@gmail.com>
2018-11-17 13:40:12 -0500

    GET Requests

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -4,43 +4,54 @@ import React, { Component } from 'react';
 
 class Test extends Component {
   state = {
-    test: 'test'
+    title: '',
+    body: ''
   };
 
   componentDidMount() {
-    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
   }
 
-  componentWillMount() {
-    console.log('componentWillMount');
-  }
+  //   componentWillMount() {
+  //     console.log('componentWillMount');
+  //   }
 
-  // Runs when something in state changes
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
+  //   // Runs when something in state changes
+  //   componentDidUpdate() {
+  //     console.log('componentDidUpdate');
+  //   }
 
-  // Runs when something in state will change
-  componentWillUpdate() {
-    console.log('componentWillUpdate');
-  }
+  //   // Runs when something in state will change
+  //   componentWillUpdate() {
+  //     console.log('componentWillUpdate');
+  //   }
 
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log('componentWillReceiveProps');
-  }
+  //   componentWillReceiveProps(nextProps, nextState) {
+  //     console.log('componentWillReceiveProps');
+  //   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    console.log('getDerivedStateFromProps');
-    return null;
-  }
+  //   static getDerivedStateFromProps(nextProps, prevState) {
+  //     console.log('getDerivedStateFromProps');
+  //     return null;
+  //   }
 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('getSnapshotBeforeUpdate');
-  }
+  //   getSnapshotBeforeUpdate(prevProps, prevState) {
+  //     console.log('getSnapshotBeforeUpdate');
+  //   }
+
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
